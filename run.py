ee5e36a6cf4274542aef07abdc8508e454f923aa
remote <competition@fudan.edu.cn>
2019-07-14 14:58:12 +0800

    update qa, ner, classification tasks

--- run.py
+++ run.py
@@ -1,24 +1,16 @@
-
+import argparse
 import torch
 
-
-# MRC
 from data.cmrc.dataset_bert import Dataset as Dataset_cmrc
 from data.drcd.dataset_bert import Dataset as Dataset_drcd
-# NER
 from data.people_daily.dataset_bert import Dataset as Dataset_peopledaily
 from data.msra.dataset_bert import Dataset as Dataset_msra
-# NLI
 from data.xnli.dataset_bert import Dataset as Dataset_xnli
-# SC
 from data.weibo.dataset_bert import Dataset as Dataset_weibo
 from data.chnsenticorp.dataset_bert import Dataset as Dataset_chnsenticorp
-# SPM
 from data.lcqmc.dataset_bert import Dataset as Dataset_lcqmc
 from data.bq_corpus.dataset_bert import Dataset as Dataset_bqcorpus
-# DC
 from data.thucnews.dataset_bert import Dataset as Dataset_thucnews
-
 
 from models.bert_models import BertForSequenceClassification, BertForSequenceLabeling, BertForQuestionAnswering
 from models.bert import BertConfig
@@ -42,76 +34,33 @@ def word_to_idx(w):
     return w2i.get(w, w2i['[UNK]'])
 
 '''
-dataset_cmrc = Dataset_cmrc(train_file='data/cmrc/cmrc2018/squad-style-data/cmrc2018_train.json', 
-                            dev_file='data/cmrc/cmrc2018/squad-style-data/cmrc2018_dev.json', 
-                            test_file='data/cmrc/cmrc2018/squad-style-data/cmrc2018_trial.json', 
-                            word_to_idx=word_to_idx)
 
-dataset_drcd = Dataset_drcd(train_file='data/drcd/DRCD/DRCD_training.json', 
-                            dev_file='data/drcd/DRCD/DRCD_dev.json', 
-                            test_file='data/drcd/DRCD/DRCD_test.json', 
-                            word_to_idx=word_to_idx)
 
-dataset_peopledaily = Dataset_peopledaily(train_file='data/people_daily/train.txt', 
-                                          dev_file='data/people_daily/dev.txt', 
-                                          test_file='data/people_daily/test.txt', 
-                                          word_to_idx=word_to_idx)
 
-dataset_msra = Dataset_msra(train_file='data/msra/msra_train_bio.txt', 
-                            test_file='data/msra/msra_test_bio.txt', 
-                            word_to_idx=word_to_idx, 
-                            split=0.9)
 
-dataset_xnli = Dataset_xnli(train_file='data/xnli/XNLI-MT-1.0/multinli/multinli.train.zh.tsv', 
-                            dev_file='data/xnli/XNLI-1.0/xnli.dev.tsv', 
-                            test_file='data/xnli/XNLI-1.0/xnli.test.tsv', 
-                            word_to_idx=word_to_idx)
 
 '''
-dataset_chnsenticorp = Dataset_chnsenticorp(train_file='data/chnsenticorp/train.tsv', 
-                                            dev_file='data/chnsenticorp/dev.tsv', 
-                                            test_file='data/chnsenticorp/test.tsv', 
-                                            word_to_idx=word_to_idx, 
-                                            max_seq_len=256)
 
-dataset_weibo = Dataset_weibo(raw_file='data/weibo/weibo_senti_100k.csv', 
-                              train_file='data/weibo/train.tsv', 
-                              dev_file='data/weibo/dev.tsv', 
-                              test_file='data/weibo/test.tsv', 
-                              word_to_idx=word_to_idx, 
-                              max_seq_len=128)
-
-'''
-dataset_lcqmc = Dataset_lcqmc(train_file='data/lcqmc/train.txt', 
-                            dev_file='data/lcqmc/dev.txt', 
-                            test_file='data/lcqmc/test.txt', 
-                            word_to_idx=word_to_idx)
-
-dataset_bqcorpus = Dataset_bqcorpus(train_file='data/bq_corpus/train.csv', 
-                                    dev_file='data/bq_corpus/dev.csv', 
-                                    test_file='data/bq_corpus/test.csv', 
-                                    word_to_idx=word_to_idx)
-
-dataset_thucnews = Dataset_thucnews(train_file='data/thucnews/cnews/cnews.train.txt',  
-                                    dev_file='data/thucnews/cnews/cnews.val.txt', 
-                                    test_file='data/thucnews/cnews/cnews.test.txt', 
-                                    word_to_idx=word_to_idx)
-'''
 config = BertConfig(json_path='../bert_checkpoints/chinese-bert_chinese_wwm_L-12_H-768_A-12/bert_config.json')
 # config = BertConfig(json_path='../bert_checkpoints/chinese_L-12_H-768_A-12/bert_config.json')
 # config = BertConfig(json_path='../bert_checkpoints/bert_toy_config.json')
 
+def run_sequence_classification(dataset, save_path, batch_size, lr, epochs=3, warmup_portion=0.1, num_save_steps=None, mini_batch_size=8):
 
-def run_sequence_classification(dataset, save_path, batch_size=10):
+    total_train_samples = epochs * dataset.num_train_samples
+    num_warmup_steps = int(total_train_samples / batch_size * warmup_portion)
+    
+    print (f'[Warmup steps] {num_warmup_steps}')
+    print (f'[Learing Rate] {lr}')
 
     model = BertForSequenceClassification(num_classes=dataset.num_classes, config=config, tf_checkpoint_path='../bert_checkpoints/chinese-bert_chinese_wwm_L-12_H-768_A-12/bert_model.ckpt')
-
     # model = BertForSequenceClassification(num_classes=dataset.num_classes, config=config)
+
     optimizer = AdamW(filter(lambda p: p.requires_grad, model.parameters()), 
-                      lr=2e-5, 
+                      lr=lr, 
                       eps=1e-6)
 
-    optimizer = WarmupOptimizer(optimizer=optimizer, num_warmup_steps=300, lr=2e-5)
+    optimizer = WarmupOptimizer(optimizer=optimizer, num_warmup_steps=num_warmup_steps, lr=lr)
 
     if torch.cuda.is_available():
         model = model.cuda()
@@ -122,16 +71,26 @@ def run_sequence_classification(dataset, save_path, batch_size=10):
                       metrics=accuracy_score, 
                       dataset=dataset, 
                       save_path=save_path, 
-                      num_save_steps=100)
+                      num_save_steps=num_save_steps)
 
-    trainer.train(batch_size=batch_size)
+    trainer.train(batch_size=batch_size, mini_batch_size=mini_batch_size)
 
-def run_sequence_labeling(dataset, save_path, batch_size=10):
+def run_sequence_labeling(dataset, save_path, batch_size, lr, epochs=3, warmup_portion=0.1, num_save_steps=None, mini_batch_size=8):
+
+    total_train_samples = epochs * dataset.num_train_samples
+    num_warmup_steps = int(total_train_samples / batch_size * warmup_portion)
+    
+    print (f'[warmup steps] {num_warmup_steps}')
+    print (f'[learing rate] {lr}')
 
     model = BertForSequenceLabeling(num_classes=dataset.num_classes, config=config, tf_checkpoint_path='../bert_checkpoints/chinese-bert_chinese_wwm_L-12_H-768_A-12/bert_model.ckpt')
+    # model = bertforsequenceClassification(num_classes=dataset.num_classes, config=config)
 
-    # model = BertForSequenceLabeling(num_classes=dataset.num_classes, config=config)
-    optimizer = AdamW(model.parameters(), lr=2e-5)
+    optimizer = AdamW(filter(lambda p: p.requires_grad, model.parameters()), 
+                      lr=lr, 
+                      eps=1e-6)
+
+    optimizer = WarmupOptimizer(optimizer=optimizer, num_warmup_steps=num_warmup_steps, lr=lr)
 
     if torch.cuda.is_available():
         model = model.cuda()
@@ -139,20 +98,30 @@ def run_sequence_labeling(dataset, save_path, batch_size=10):
 
     trainer = Trainer(model=model, 
                       optimizer=optimizer, 
-                      # NOTE should try other
+                      # FIXME should try other metrics
                       metrics=ner_f1_score, 
                       dataset=dataset, 
-                      save_path=save_path)
+                      save_path=save_path, 
+                      num_save_steps=num_save_steps)
 
-    trainer.train(batch_size=batch_size)
+    trainer.train(batch_size=batch_size, mini_batch_size=mini_batch_size)
 
+def run_question_answering(dataset, save_path, batch_size, lr, epochs=3, warmup_portion=0.1, num_save_steps=None, mini_batch_size=8):
 
-def run_question_answering(dataset, save_path, batch_size=10):
+    total_train_samples = epochs * dataset.num_train_samples
+    num_warmup_steps = int(total_train_samples / batch_size * warmup_portion)
     
-    model = BertForQuestionAnswering(config=config, tf_checkpoint_path='../bert_checkpoints/chinese-bert_chinese_wwm_L-12_H-768_A-12/bert_model.ckpt')
+    print (f'[Warmup steps] {num_warmup_steps}')
+    print (f'[Learing Rate] {lr}')
 
-    # model = BertForQuestionAnswering(config=config)
-    optimizer = AdamW(model.parameters(), lr=1e-5)
+    model = BertForQuestionAnswering(config=config, tf_checkpoint_path='../bert_checkpoints/chinese-bert_chinese_wwm_L-12_H-768_A-12/bert_model.ckpt')
+    # model = BertForSequenceClassification(num_classes=dataset.num_classes, config=config)
+
+    optimizer = AdamW(filter(lambda p: p.requires_grad, model.parameters()), 
+                      lr=lr, 
+                      eps=1e-6)
+
+    optimizer = WarmupOptimizer(optimizer=optimizer, num_warmup_steps=num_warmup_steps, lr=lr)
 
     if torch.cuda.is_available():
         model = model.cuda()
@@ -160,16 +129,141 @@ def run_question_answering(dataset, save_path, batch_size=10):
 
     trainer = Trainer(model=model, 
                       optimizer=optimizer, 
-                      # NOTE try other metrics
-                      metrics=qa_f1_score, 
+                      # FIXME should try other metrics
+                      metrics=qa_em_score, 
                       dataset=dataset, 
-                      save_path=save_path)
+                      save_path=save_path, 
+                      num_save_steps=num_save_steps)
 
-    trainer.train(batch_size=batch_size)
+    trainer.train(batch_size=batch_size, mini_batch_size=mini_batch_size)
 
 
 if __name__ == '__main__':
-    run_sequence_classification(dataset_chnsenticorp, batch_size=10, save_path='checkpoints/chnsenticorp.pt')
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--device', required=False, default=0)
+    parser.add_argument('-t', '--task', required=True)
+    parser.add_argument('-b', '--batch_size', required=False, default=64)
+    parser.add_argument('-l', '--learning_rate', required=False, default=2e-5)
+    # parser.add_argument('-mb', '--mini_batch_size', required=False, default=8)
 
+    args = parser.parse_args()
+
+    device = int(args.device)
+    task = args.task
+    batch_size = int(args.batch_size)
+    # mini_batch_size = int(args.mini_batch_size)
+    lr = float(args.learning_rate)
+
+    torch.cuda.set_device(device)
+
+    save_path = f'checkpoints/{task}_lr{lr}_b{batch_size}_{device}.pt'
+
+    if task == 'cmrc':
+        dataset = Dataset_cmrc(train_file='data/cmrc/cmrc2018/squad-style-data/cmrc2018_train.json', 
+                               dev_file='data/cmrc/cmrc2018/squad-style-data/cmrc2018_dev.json', 
+                               test_file='data/cmrc/cmrc2018/squad-style-data/cmrc2018_trial.json', 
+                               word_to_idx=word_to_idx, 
+                               max_seq_len=512)
+
+        # mini-batch 1 to prevent OOM
+        run_question_answering(dataset, batch_size=batch_size, lr=lr, epochs=2, save_path=save_path, num_save_steps=50, mini_batch_size=1)
+
+    elif task == 'drcd':
+        dataset = Dataset_drcd(train_file='data/drcd/DRCD/DRCD_training.json', 
+                              dev_file='data/drcd/DRCD/DRCD_dev.json', 
+                              test_file='data/drcd/DRCD/DRCD_test.json', 
+                              word_to_idx=word_to_idx, 
+                              max_seq_len=512)
+
+        # mini-batch 1 to prevent OOM
+        run_question_answering(dataset, batch_size=batch_size, lr=lr, epochs=2, save_path=save_path, num_save_steps=100, mini_batch_size=1)
+
+    elif task == 'people_daily' or task == 'peopledaily':
+        dataset = Dataset_peopledaily(train_file='data/people_daily/train.txt', 
+                                      dev_file='data/people_daily/dev.txt', 
+                                      test_file='data/people_daily/test.txt', 
+                                      word_to_idx=word_to_idx, 
+                                      max_seq_len=256)
+        # FIXME num_save_steps
+        run_sequence_labeling(dataset, batch_size=batch_size, lr=lr, epochs=2, save_path=save_path, num_save_steps=100)
+
+    elif task == 'msra' or task == 'msra_ner':
+
+        dataset = Dataset_msra(train_file='data/msra/msra_train_bio.txt', 
+                               test_file='data/msra/msra_test_bio.txt', 
+                               word_to_idx=word_to_idx, 
+                               split=0.9, 
+                               max_seq_len=256)
+        run_sequence_labeling(dataset, batch_size=batch_size, lr=lr, epochs=2, save_path=save_path, num_save_steps=100)
+
+    # OK
+    elif task == 'xnli':
+        dataset = Dataset_xnli(train_file='data/xnli/XNLI-MT-1.0/multinli/multinli.train.zh.tsv', 
+                               dev_file='data/xnli/XNLI-1.0/xnli.dev.tsv', 
+                               test_file='data/xnli/XNLI-1.0/xnli.test.tsv', 
+                               word_to_idx=word_to_idx, 
+                               max_seq_len=128)
+
+        run_sequence_classification(dataset, batch_size=batch_size, lr=lr, epochs=2, save_path=save_path, num_save_steps=100)
+
+    # OK
+    elif task == 'chnsenticorp':
+
+        dataset = Dataset_chnsenticorp(train_file='data/chnsenticorp/train.tsv', 
+                                       dev_file='data/chnsenticorp/dev.tsv', 
+                                       test_file='data/chnsenticorp/test.tsv', 
+                                       word_to_idx=word_to_idx, 
+                                       max_seq_len=256)
+
+        run_sequence_classification(dataset, batch_size=batch_size, lr=lr, epochs=2, save_path=save_path)
+
+    # OK
+    elif task == 'weibo' or task == 'sina_weibo':
+
+        dataset = Dataset_weibo(raw_file='data/weibo/weibo_senti_100k.csv', 
+                                train_file='data/weibo/train.tsv', 
+                                dev_file='data/weibo/dev.tsv', 
+                                test_file='data/weibo/test.tsv', 
+                                word_to_idx=word_to_idx, 
+                                max_seq_len=128)
+
+        run_sequence_classification(dataset, batch_size=batch_size, lr=lr, epochs=2, save_path=save_path, num_save_steps=100)
+
+    # OK
+    elif task == 'lcqmc':
+
+        dataset = Dataset_lcqmc(train_file='data/lcqmc/train.txt', 
+                                dev_file='data/lcqmc/dev.txt', 
+                                test_file='data/lcqmc/test.txt', 
+                                word_to_idx=word_to_idx, 
+                                max_seq_len=128)
+
+        run_sequence_classification(dataset, batch_size=batch_size, lr=lr, epochs=2, save_path=save_path, num_save_steps=100)
+
+    # OK
+    elif task == 'bq_corpus' or task == 'bqcorpus':
+
+        dataset = Dataset_bqcorpus(train_file='data/bq_corpus/train.csv', 
+                                   dev_file='data/bq_corpus/dev.csv', 
+                                   test_file='data/bq_corpus/test.csv', 
+                                   word_to_idx=word_to_idx, 
+                                   max_seq_len=128)
+
+        run_sequence_classification(dataset, batch_size=batch_size, lr=lr, epochs=2, save_path=save_path)
+    
+    # OK
+    elif task == 'thucnews':
+
+        dataset = Dataset_thucnews(raw_train_file='data/thucnews/cnews/cnews.train.txt',  
+                                   train_file='data/thucnews/cnews/cnews.train.shuffled.txt',  
+                                   dev_file='data/thucnews/cnews/cnews.val.txt', 
+                                   test_file='data/thucnews/cnews/cnews.test.txt', 
+                                   word_to_idx=word_to_idx, 
+                                   max_seq_len=512)
+
+        # mini-batch 1 to prevent OOM
+        run_sequence_classification(dataset, batch_size=batch_size, lr=lr, epochs=2, save_path=save_path, num_save_steps=100, mini_batch_size=1)
+        
     
 
--- trainer.py
+++ trainer.py
@@ -2,32 +2,35 @@ import torch
 
 class Trainer():
 
-    def __init__(self, model, optimizer, metrics, dataset, save_path, num_save_steps=100):
+    def __init__(self, model, optimizer, metrics, dataset, save_path, num_save_steps):
         # metrics: function to compute accuracy for early-stop
 
         self.dataset = dataset
 
-        self.num_save_steps = num_save_steps
 
         self.model = model
         self.optimizer = optimizer
 
         self.metrics = metrics
 
+        self.num_save_steps = num_save_steps
         self.save_path = save_path
         self.save_path_tmp = save_path + '.tmp'
 
 
-    # FIXME should modify both load and save function when adding other objects to save
+    # NOTE should modify both load and save functions
     def load_checkpoint(self):
         try:
             cp = torch.load(self.save_path)
 
-            self.model.load_state_dict(cp['model'])
-            self.optimizer.load_state_dict(cp['optimizer'])
-            self.best_score = cp['best_score']
             self.num_trained_samples = cp['num_trained_samples']
+            self.best_score = cp['best_score']
+            self.optimizer.load_state_dict(cp['optimizer'])
+            self.model.load_state_dict(cp['model'])
+
             print ('Load checkpoint successfully!')
+            print (f'[Best score] {self.best_score}')
+            print (f'[Trained samples] {self.num_trained_samples}')
         except:
             self.best_score = 0
             self.num_trained_samples = 0
@@ -47,60 +50,77 @@ class Trainer():
 
         print ('Finish.')
 
-    def train(self, batch_size):
+    def train(self, batch_size, mini_batch_size=8):
+
+        try:
+            assert batch_size % mini_batch_size == 0
+        except: 
+            print ('Please set batch size to a multiple of 8 to enable accumulated gradient!')
+            raise
+        
+        accumulation_steps = batch_size // mini_batch_size
 
         self.load_checkpoint()
-
+        old_num_trained_samples = self.num_trained_samples
 
         step = 0
+
+        if self.num_save_steps is None:
+            num_save_steps = int(0.1 * self.dataset.num_train_samples / batch_size)
+        else:
+            num_save_steps = self.num_save_steps
+        print (f'number of save steps: {num_save_steps}')
 
         stop_after_overfit = 10
         cnt_overfit = 0
 
         while True:
 
-            cnt = 0
-           
-            for batch in self.dataset.trainset(batch_size=batch_size, drop_last=True):
+            self.model.zero_grad()
+            loss_sum = 0
 
+            for i, batch in enumerate(self.dataset.trainset(batch_size=mini_batch_size, drop_last=True)):
+                
                 self.model.train()
-                self.model.zero_grad()
+
                 # NOTE Sequence labeling should consider mask !
-                loss = self.model.compute_loss(self.model(batch.input), batch.target)
+                loss = self.model.compute_loss(self.model(batch.input), batch.target) / accumulation_steps
                 loss.backward()
+                loss_sum += loss
+
+                if (i + 1) % accumulation_steps != 0:
+                    continue
+
                 self.optimizer.step()
-
-                print (f'idx: {cnt}, loss: {loss}')
-
-                cnt += batch_size
+                self.model.zero_grad()
                 step += 1
+                print (f'idx: {(i+1) * mini_batch_size}, loss: {loss_sum}')
+                loss_sum = 0
 
-                if step >= self.num_save_steps: 
-
+                if step % num_save_steps == 0: 
 
                     self.model.eval()
-                    score = self.metrics(self.model, self.dataset.devset)
+                    with torch.no_grad():
+                        score = self.metrics(self.model, self.dataset.devset, batch_size=mini_batch_size)
 
                     print (f'score: {score}')
 
                     if score > self.best_score:
+                        print (f'[Best score] {score}')
 
                         self.best_score = score
-                        self.num_trained_samples += step * batch_size
+                        self.num_trained_samples = old_num_trained_samples + step * batch_size
                         self.save_checkpoint(self.save_path)
                         cnt_overfit = 0
 
                     else:
+                        print (f'[Best score] {self.best_score}')
+
                         cnt_overfit += 1
                         if cnt_overfit >= stop_after_overfit:
                             print ('Terminate!')
+                            print (f'[Trained samples] {self.num_trained_samples}')
                             return 
-
-                    step = 0
-            # print (f'Epoch: {self.num_trained_samples // self.dataset.num_train_samples}, score: {score}')
-
-
-
 
 if __name__ == '__main__':
        
